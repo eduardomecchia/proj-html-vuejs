3d2d4fef436bda4e12332f811a1c10847f6d8324
Eduardo Mecchia <eduardomecchia273@gmail.com>
2021-06-15 14:20:39 +0200

    Add array of features to be cycled through in section #3

--- dist/js/app.js
+++ dist/js/app.js
@@ -10,7 +10,24 @@
 var app = new Vue({
   el: "#app",
   data: {
-    navigationLinks: ["home", "about us", "facilities", "membership", "testimonials", "blog"]
+    navigationLinks: ["home", "about us", "facilities", "membership", "testimonials", "blog"],
+    thirdSectionFeatures: [{
+      name: "Strength & Conditioning",
+      iconClass: "fa-unlink",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In augue nisl, ornare es volutpat etra us."
+    }, {
+      name: "Fitness & Cardio",
+      iconClass: "fa-bicycle",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In augue nisl, ornare es volutpat etra us."
+    }, {
+      name: "Flexibility & Rest",
+      iconClass: "",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In augue nisl, ornare es volutpat etra us."
+    }, {
+      name: "Health & Diet",
+      iconClass: "fa-heartbeat",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In augue nisl, ornare es volutpat etra us."
+    }]
   },
   methods: {}
 });
