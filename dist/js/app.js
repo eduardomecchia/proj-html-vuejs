333713d40a1432115e2c376d240edbbbe6206a89
Eduardo Mecchia <eduardomecchia273@gmail.com>
2021-06-15 14:42:10 +0200

    Add array of feedbacks to Vue.js

--- dist/js/app.js
+++ dist/js/app.js
@@ -27,6 +27,23 @@ var app = new Vue({
       name: "Health & Diet",
       iconClass: "fa-heartbeat",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In augue nisl, ornare es volutpat etra us."
+    }],
+    fourthSectionFeedbacks: [{
+      author: "Tara Smith",
+      feedback: "The trainers at Avada Gym have helped me reach my fitness goals beyond anything I could of hoped for. I love it!",
+      image: "./dist/img/review_1-compressor.jpg"
+    }, {
+      author: "Simon Chel",
+      feedback: "The atmosphere at Avada Gym is amazing, the people are even better. All in all, it is a great place to work out.",
+      image: "./dist/img/review_2-compressor.jpg"
+    }, {
+      author: "Jen Wirth",
+      feedback: "The facilities at Avada Gym stood out to me the most. They are well maintained and have high quality equipment.",
+      image: "./dist/img/review_3-compressor-2.jpg"
+    }, {
+      author: "Jeff Glum",
+      feedback: "No bad things to say about Avada Gym, they are top notch at every corner and truly take care of their customers.",
+      image: "./dist/img/review_4-compressor-2.jpg"
     }]
   },
   methods: {}
