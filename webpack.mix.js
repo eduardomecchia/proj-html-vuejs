144271d763bf6469c0423c4aa01b7a847c133890
Eduardo Mecchia <eduardomecchia273@gmail.com>
2021-06-15 10:45:36 +0200

    Start styling header and jumbotron

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,6 +4,6 @@ const mix = require('laravel-mix');
 
 mix
 .js('src/app.js', 'js')
-.sass('src/app.scss', 'css')
+.sass('src/app.scss', 'css').options({ processCssUrls: false })
 .copyDirectory('src/img', 'dist/img')
 .setPublicPath('dist');
